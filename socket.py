1d9366d3464a107993c0c1b899bd51e433352be1
Elijah Roberts <eroberts@verifiedfirst.com>
2019-11-04 08:40:30 -0700

    Update socket.py
    fixing loop to work off of time not number of questions

--- socket.py
+++ socket.py
@@ -1,18 +1,16 @@
 import socket
 import base64
+import time
 
 PORT = 4055
 HOST = "challenges.neverlanctf.com"
+SECONDS = 10
 
 def main():
-"""
-This CTF event required us to connect to a port, decode the text response, and send it back multiple times in under 10 seconds.
-"""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    request = f"GET / HTTP/1.1\nHost: {HOST}\n\n".encode()
     s.connect((HOST, PORT))
-    questions = 0
-    while questions < 10:
+    time_end = time.time() + SECONDS
+    while time.time() < time_end:
         result = s.recv(10000)
         print(result)
         encoded_string = result.decode("utf-8").split("\n")[-1].split(':')[-1].strip()
@@ -20,7 +18,7 @@ This CTF event required us to connect to a port, decode the text response, and s
         decoded_string = base64.b64decode(encoded_string).decode("UTF-8")
         print(decoded_string)
         s.send(bytes(decoded_string, 'utf-8'))
-        questions +=  1
+    
 
 
 if __name__ == '__main__':
